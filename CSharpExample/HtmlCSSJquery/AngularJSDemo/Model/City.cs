acf475a64dde52c9e087a790ca586475b8b982d7
研发中心-胡睿毅06464 <hry6464@tcent.cn>
2016-11-21 18:26:00 +0800

    AngularJSDemo

---
+++ CSharpExample/HtmlCSSJquery/AngularJSDemo/Model/City.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularJSDemo.Model
+{
+    public class City
+    {
+        public long ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string CountryCode { get; set; }
+
+        public string District { get; set; }
+
+        public long Population { get; set; }
+    }
+}
---
+++ CSharpExample/HtmlCSSJquery/AngularJSDemo/Service/Demo.ashx.cs
@@ -0,0 +1,67 @@
+﻿using AngularJSDemo.Model;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace AngularJSDemo.Service
+{
+    /// <summary>
+    /// Demo 的摘要说明
+    /// </summary>
+    public class Demo : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            string action = context.Request["action"];
+            switch (action)
+            {
+                case "GetCountrys":
+                    GetCountrys(context);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void GetCountrys(HttpContext context)
+        {
+            List<City> response = new List<City>();
+            using (MySqlConnection con = new MySqlConnection())
+            {
+                con.ConnectionString = "server=127.0.0.1;uid=root;pwd=root;database=world";
+                con.Open();
+                string cmdText = "SELECT * FROM world.city limit 0 ,15";
+                using (MySqlCommand cmd = new MySqlCommand(cmdText, con))
+                {
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    if (reader != null)
+                    {
+                        while (reader.Read())
+                        {
+                            response.Add(new City
+                            {
+                                ID = Convert.ToInt64(reader["ID"]),
+                                CountryCode = reader["CountryCode"].ToString(),
+                                District = reader["District"].ToString(),
+                                Name = reader["Name"].ToString(),
+                                Population = Convert.ToInt64(reader["Population"])
+                            });
+                        }
+                    }
+                }
+            }
+            context.Response.Write(JsonConvert.SerializeObject(response));
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
