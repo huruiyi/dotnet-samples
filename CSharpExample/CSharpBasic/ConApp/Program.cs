fc452dd6d6cd8aec880018920c14c49d4116b4a7
胡睿毅 <807776962@qq.com>
2017-07-29 15:02:04 +0800

    Descartes

--- CSharpExample/CSharpBasic/ConApp/Program.cs
+++ CSharpExample/CSharpBasic/ConApp/Program.cs
@@ -3188,5 +3188,45 @@ namespace ConApp
                 Console.WriteLine(item.GetNativeOffset());
             }
         }
+
+
+    }
+
+    public class Descartes
+    {
+        public static void run(List<List<string>> dimvalue, List<string> result, int layer, string curstring)
+        {
+            if (layer < dimvalue.Count - 1)
+            {
+                if (dimvalue[layer].Count == 0)
+                {
+                    run(dimvalue, result, layer + 1, curstring);
+                }
+                else
+                {
+                    for (int i = 0; i < dimvalue[layer].Count; i++)
+                    {
+                        StringBuilder s1 = new StringBuilder();
+                        s1.Append(curstring);
+                        s1.Append(dimvalue[layer][i]);
+                        run(dimvalue, result, layer + 1, s1.ToString());
+                    }
+                }
+            }
+            else if (layer == dimvalue.Count - 1)
+            {
+                if (dimvalue[layer].Count == 0)
+                {
+                    result.Add(curstring);
+                }
+                else
+                {
+                    for (int i = 0; i < dimvalue[layer].Count; i++)
+                    {
+                        result.Add(curstring + dimvalue[layer][i]);
+                    }
+                }
+            }
+        }
     }
 }
