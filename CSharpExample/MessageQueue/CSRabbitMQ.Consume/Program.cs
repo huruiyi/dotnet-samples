a991dd0e30c1b03bef2418723b6f7119c3296379
胡睿毅 <807776962@qq.com>
2017-09-06 00:31:04 +0800

    RabbitMQ

---
+++ CSharpExample/MessageQueue/CSRabbitMQ.Consume/Program.cs
@@ -0,0 +1,56 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+//http://www.rabbitmq.com/tutorials/tutorial-one-dotnet.html
+namespace CSRabbitMQ.Consume
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            RabbitMQ.Client.ConnectionFactory connectionFactory = new RabbitMQ.Client.ConnectionFactory();
+            connectionFactory.UserName = "admin";
+            connectionFactory.Password = "admin";
+
+            using (IConnection connection = connectionFactory.CreateConnection())
+            {
+                connection.ConnectionShutdown += Connection_ConnectionShutdown;
+
+                Console.WriteLine(connection.IsOpen);
+                Console.WriteLine(connection.RemotePort);
+
+                using (IModel channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                                queue: "hello",
+                                durable: false,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+                    for (int i = 0; i < 1000; i++)
+                    {
+                        string message = string.Format("Hello World!I am Num {0}", i);
+                        var body = Encoding.UTF8.GetBytes(message);
+                        Thread.Sleep(2000);
+                        channel.BasicPublish(
+                           exchange: "",
+                           routingKey: "hello",
+                           basicProperties: null,
+                           body: body);
+                        Console.WriteLine(" [x] Sent {0}", message);
+                    }
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        private static void Connection_ConnectionShutdown(object sender, ShutdownEventArgs e)
+        {
+            Console.WriteLine(e.ReplyText);
+        }
+    }
+}
---
+++ CSharpExample/MessageQueue/CSRabbitMQ.Publish/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+//http://www.rabbitmq.com/tutorials/tutorial-one-dotnet.html
+namespace CSRabbitMQ.Publish
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //RabbitMQ.Client.ConnectionFactory connectionFactory = new RabbitMQ.Client.ConnectionFactory();
+            //connectionFactory.UserName = "admin";
+            //connectionFactory.Password = "admin";
+            ConnectionFactory connectionFactory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (IConnection connection = connectionFactory.CreateConnection())
+            {
+                connection.ConnectionShutdown += Connection_ConnectionShutdown;
+
+                Console.WriteLine(connection.IsOpen);
+                Console.WriteLine(connection.RemotePort);
+
+                using (IModel channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                  durable: false,
+                                  exclusive: false,
+                                  autoDelete: false,
+                                  arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                    };
+                    channel.BasicConsume(queue: "hello",
+                                         autoAck: true,
+                                         consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        private static void Connection_ConnectionShutdown(object sender, ShutdownEventArgs e)
+        {
+            Console.WriteLine(e.ReplyText);
+        }
+    }
+}
