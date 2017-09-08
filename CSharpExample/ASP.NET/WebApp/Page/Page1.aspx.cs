ba04f8bfdab30c2b632d579f9169b3ffa1d34278
胡睿毅 <807776962@qq.com>
2017-09-09 02:34:17 +0800

    Html

--- CSharpExample/ASP.NET/WebApp/Page/Page1.aspx.cs
+++ CSharpExample/ASP.NET/WebApp/Page/Page1.aspx.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
 
 namespace WebApp.page
 {
@@ -6,6 +8,21 @@ namespace WebApp.page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Response.Write(
+                  "<br>版本号的主版本号：" + Request.Browser.ClrVersion.Major.ToString()
+                  + "<br>版本号的次版本号：" + Request.Browser.ClrVersion.Minor.ToString()
+                  + "<br>修定号的高16位：" + Request.Browser.ClrVersion.MajorRevision.ToString()
+                  + "<br>修定号的低16位：" + Request.Browser.ClrVersion.MinorRevision.ToString()
+                  + "<br>内部版本号部分值(Build)：" + Request.Browser.ClrVersion.Build.ToString()
+                  + "<br>版本号的修定号部分的值(Revision)：" + Request.Browser.ClrVersion.Revision.ToString()
+                  );
+            string frameworkInstallDir = RuntimeEnvironment.GetRuntimeDirectory();
+
+            string sysVersion = RuntimeEnvironment.GetSystemVersion();
+            Evidence evidence = new Evidence();
+
+            System.Security.HostSecurityManager hs = new System.Security.HostSecurityManager();
+
             Label1.Text = "Page=" + Request.QueryString["page"] + " PathInfo=" + Request.PathInfo;
         }
     }
