9e6f795641f6b57d4278e2bfff5ba4a145abca36
胡睿毅 <807776962@qq.com>
2017-10-31 02:00:40 +0800

    .NetCore

---
+++ CSharpExample/MVCDemo/NetCore.Sample/Models/LibraryDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace NetCore.Sample.Models
+{
+    public class LibraryDetails
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string BookName { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ CSharpExample/MVCDemo/NetCore.Sample/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace NetCore.Sample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IWebHost host = new WebHostBuilder()
+              .UseKestrel()
+              .UseContentRoot(Directory.GetCurrentDirectory())
+              .UseIISIntegration()
+              .UseStartup<Startup>()
+              .Build();
+
+            host.Run();
+
+            //BuildWebHost(args).Run();
+        }
+
+        //public static IWebHost BuildWebHost(string[] args)
+        //    => WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build();
+    }
+}
