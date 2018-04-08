e97a7b4188dd2e14928b45f1f12759159aa20658
胡睿毅 <807776962@qq.com>
2018-04-08 14:28:58 +0800

    XMLDemo

--- CSharpExample/CSharpBasic/ConApp/Samples/LinqDemo.cs
+++ CSharpExample/CSharpBasic/ConApp/Samples/LinqDemo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace System.Collections.Generic
 {
@@ -75,6 +76,43 @@ namespace ConApp.Samples
             }
 
             #endregion 扩展方法
+        }
+
+        public static void LambaExpressions()
+        {
+            string[] names = { "John", "Paul", "George", "Ringo" };
+            var name = names.Select(s => s.StartsWith("P"));
+
+            int[] numbers = { 1, 20, 15, 35, 42, 18, 99 };
+            var nums = numbers.OrderBy(x => x);
+            var numsReverse = numbers.OrderBy(x => x).Reverse();
+        }
+
+        public static void CurrentCamp()
+        {
+            string[] camps = { "Code Camp 2007", "Code Camp 2008", "Code Camp 2009" };
+            var currentCamp = from camp in camps
+                              where camp.EndsWith(DateTime.Now.Year.ToString())
+                              select camp;
+
+            // These are all equivalent
+            string currentCamp2 = camps.Where(c => c.EndsWith(DateTime.Now.Year.ToString())).Single();
+            string currentCamp3 = camps.Single(c => c.EndsWith(DateTime.Now.Year.ToString()));
+            string currentCamp4 = camps.Select(c => c).Where(c => c.EndsWith(DateTime.Now.Year.ToString())).Single();
+        }
+
+        public static void LambaExpressionsWithDelegates()
+        {
+            Func<string, bool> pOnly = delegate (string s)
+            {
+                return s.StartsWith("P");
+            };
+            string[] names = { "John", "Paul", "George", "Ringo" };
+
+            var name1 = names.Select(pOnly);
+
+            string name2 = names.Single(s => s.StartsWith("P"));
+            string name3 = names.Single(pOnly);
         }
     }
 }
--- CSharpExample/CSharpBasic/ConApp/Samples/XMLDemo.cs
+++ CSharpExample/CSharpBasic/ConApp/Samples/XMLDemo.cs
@@ -3,6 +3,7 @@ using ConApp.Properties;
 using Net.Tools;
 using System;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Xml;
 using System.Xml.Linq;
@@ -387,5 +388,315 @@ namespace ConApp
             XmlNode xmlNode1 = doc.SelectSingleNode("Order/Items");
             Console.WriteLine(xmlNode1.InnerXml);
         }
+
+        #region XElement Demo
+
+        public static void CreateEmployees()
+        {
+            XDocument doc = new XDocument(
+                // To use a XElement the XDeclaration and XCooment can't be used
+                //XElement doc = new XElement(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XComment("A sample xml file"),
+                new XElement("employees",
+                    new XElement("employee",
+                        new XAttribute("id", 1),
+                        new XAttribute("salaried", "false"),
+                        new XElement("name", "Gustavo Achong"),
+                        new XElement("hire_date", "7/31/1996")),
+                    new XElement("employee",
+                        new XAttribute("id", 3),
+                        new XAttribute("salaried", "true"),
+                        new XElement("name", "Kim Abercrombie"),
+                        new XElement("hire_date", "12/12/1997")),
+                    new XElement("employee",
+                        new XAttribute("id", 8),
+                        new XAttribute("salaried", "false"),
+                        new XElement("name", "Carla Adams"),
+                        new XElement("hire_date", "2/6/1998")),
+                    new XElement("employee",
+                        new XAttribute("id", 9),
+                        new XAttribute("salaried", "false"),
+                        new XElement("name", "Jay Adams"),
+                        new XElement("hire_date", "2/6/1998"))
+                )
+            );
+
+            Console.WriteLine(doc.ToString());
+
+            doc.Save(@"f:\test.xml");
+        }
+
+        public static void SingleEmployee()
+        {
+            XElement employee = new XElement("employee",
+                new XAttribute("id", 1),
+                new XAttribute("salaried", "false"),
+                new XElement("name", "Gustavo Achong"),
+                new XElement("hire_date", "7/31/1996"));
+
+            Console.WriteLine(employee.ToString());
+        }
+
+        public static void WithNamespace()
+        {
+            XNamespace ns = "http://mycompany.com";
+
+            XElement doc = new XElement(
+                new XElement(ns + "employees",
+                    new XElement("employee",
+                        new XAttribute("id", 1),
+                        new XAttribute("salaried", "false"),
+                        // Note here that a namespace is not supplied on these
+                        // elements. This will cause an emoty namespace to be
+                        // included
+                        new XElement("name", "Gustavo Achong"),
+                        new XElement("hire_date", "7/31/1996")),
+                    new XElement(ns + "employee",
+                        new XAttribute("id", 3),
+                        new XAttribute("salaried", "true"),
+                        new XElement("name", "Kim Abercrombie"),
+                        new XElement("hire_date", "12/12/1997")),
+                    new XElement(ns + "employee",
+                        new XAttribute("id", 8),
+                        new XAttribute("salaried", "false"),
+                        new XElement("name", "Carla Adams"),
+                        new XElement("hire_date", "2/6/1998")),
+                    new XElement(ns + "employee",
+                        new XAttribute("id", 9),
+                        new XAttribute("salaried", "false"),
+                        new XElement("name", "Jay Adams"),
+                        new XElement("hire_date", "2/6/1998"))
+                )
+            );
+            Console.WriteLine(doc.ToString());
+        }
+
+        public static void WithNamespaceWithPrefix()
+        {
+            XNamespace ns1 = "http://mycompany.com";
+            XNamespace ns2 = "http://somecompany.com";
+
+            XElement doc = new XElement(
+                  new XElement(ns1 + "employees",
+                  new XAttribute(XNamespace.Xmlns + "ns1", ns1),
+                     new XElement(ns2 + "employee",
+                         new XAttribute("id", 1),
+                         new XAttribute("salaried", "false"),
+                         new XAttribute(XNamespace.Xmlns + "ns2", ns2),
+                             new XElement(ns2 + "name", "Gustavo Achong"),
+                             new XElement(ns2 + "hire_date", "7/31/1996")),
+                     new XElement(ns2 + "employee",
+                         new XAttribute("id", 3),
+                         new XAttribute("salaried", "true"),
+                         new XAttribute(XNamespace.Xmlns + "ns2", ns2),
+                             new XElement(ns2 + "name", "Kim Abercrombie"),
+                             new XElement(ns2 + "hire_date", "12/12/1997")),
+                     new XElement(ns2 + "employee",
+                         new XAttribute("id", 8),
+                         new XAttribute("salaried", "false"),
+                         new XAttribute(XNamespace.Xmlns + "ns2", ns2),
+                            new XElement(ns2 + "name", "Carla Adams"),
+                             new XElement(ns2 + "hire_date", "2/6/1998")),
+                     new XElement(ns2 + "employee",
+                         new XAttribute("id", 9),
+                         new XAttribute("salaried", "false"),
+                         new XAttribute(XNamespace.Xmlns + "ns2", ns2),
+                             new XElement(ns2 + "name", "Jay Adams"),
+                             new XElement(ns2 + "hire_date", "2/6/1998"))
+                         )
+                        );
+            Console.WriteLine(doc.ToString());
+        }
+
+        public static void Casting()
+        {
+            XElement element1 = new XElement("number", 42);
+            // It doesn't matter it the value is a string or int
+            XElement element2 = new XElement("number", "42");
+
+            int num1 = (int)element1;
+            int num2 = (int)element2;
+        }
+
+        public static void TraverseTree()
+        {
+            XDocument doc = CreateEmployeesPrivate();
+
+            Console.WriteLine("-- All nodes --");
+
+            // Iterate over all nodes
+            foreach (var node in doc.Nodes())
+            {
+                Console.WriteLine(node);
+            }
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("-- XElement nodes --");
+
+            // Iterate over the XElement nodes
+            foreach (var node in doc.Nodes().OfType<XElement>())
+            {
+                Console.WriteLine(node);
+            }
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("-- XComment nodes --");
+
+            // Iterate over only the XComment nodes
+            foreach (var node in doc.Nodes().OfType<XComment>())
+            {
+                Console.WriteLine(node);
+            }
+
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine("-- Employee name nodes --");
+
+            // Iterate over only the XComment nodes
+            foreach (var node in doc.Elements("employees").Elements("employee").Elements("name"))
+            {
+                Console.WriteLine(node);
+            }
+        }
+
+        public static void CreateFromQuery()
+        {
+            XDocument doc = CreateEmployeesPrivate();
+
+            // Order the employee elements by hire date and select
+            // the first one as the newest employee
+            var newElement = doc.Descendants("employee").Select(e => e).OrderBy(e => DateTime.Parse((string)e.Element("hire_date"))).First();
+
+            // Create an element from the above query
+            XElement element = new XElement("newest_hire", newElement);
+
+            Console.WriteLine(element);
+        }
+
+        public static void Transform()
+        {
+            XDocument doc = CreateEmployeesPrivate();
+
+            // Use query expression to find all salaried employees
+            // and create a new XElement with their name(s)
+            XElement element = new XElement("salaried_employees", from e in doc.Descendants("employee")
+                                                                  where e.Attribute("salaried").Value == "true"
+                                                                  select new XElement("employee",
+                                                                      new XElement(e.Element("name")))
+            );
+            Console.WriteLine(element);
+        }
+
+        private static XDocument CreateEmployeesPrivate()
+        {
+            XDocument doc = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XComment("A sample xml file"),
+                new XElement("employees",
+                    new XElement("employee",
+                        new XAttribute("id", 1),
+                        new XAttribute("salaried", "false"),
+                        new XElement("name", "Gustavo Achong"),
+                        new XElement("hire_date", "7/31/1996")),
+                    new XElement("employee",
+                        new XAttribute("id", 3),
+                        new XAttribute("salaried", "true"),
+                        new XElement("name", "Kim Abercrombie"),
+                        new XElement("hire_date", "12/12/1997")),
+                    new XElement("employee",
+                        new XAttribute("id", 8),
+                        new XAttribute("salaried", "false"),
+                        new XElement("name", "Carla Adams"),
+                        new XElement("hire_date", "2/6/1998")),
+                    new XElement("employee",
+                        new XAttribute("id", 9),
+                        new XAttribute("salaried", "false"),
+                        new XElement("name", "Jay Adams"),
+                        new XElement("hire_date", "2/6/1998"))
+                )
+            );
+
+            return doc;
+        }
+
+        #endregion XElement Demo
+
+        #region XmlElement Demo Old
+
+        private static XmlDocument m_doc = new XmlDocument();
+
+        public static void CreateEmployees_Old()
+        {
+            XmlElement root = m_doc.CreateElement("employees");
+            root.AppendChild(AddEmployee(1, "Gustavo Achong", DateTime.Parse("7/31/1996"), false));
+            root.AppendChild(AddEmployee(3, "Kim Abercrombie", DateTime.Parse("12/12/1997"), true));
+            root.AppendChild(AddEmployee(8, "Carla Adams", DateTime.Parse("2/6/1998"), false));
+            root.AppendChild(AddEmployee(9, "Jay Adams", DateTime.Parse("2/6/1998"), false));
+
+            m_doc.AppendChild(root);
+
+            Console.WriteLine(m_doc.OuterXml);
+        }
+
+        private static XmlElement AddEmployee(int ID, string name, DateTime hireDate, bool isSalaried)
+        {
+            XmlElement employee = m_doc.CreateElement("employee");
+
+            XmlElement nameElement = m_doc.CreateElement("name");
+            nameElement.InnerText = name;
+
+            XmlElement hireDateElement = m_doc.CreateElement("hire_date");
+            hireDateElement.InnerText = hireDate.ToShortDateString();
+
+            employee.SetAttribute("id", ID.ToString());
+            employee.SetAttribute("salaried", isSalaried.ToString());
+            employee.AppendChild(nameElement);
+            employee.AppendChild(hireDateElement);
+
+            return employee;
+        }
+
+        public static void Casting_Old()
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlElement root = doc.CreateElement("employees");
+            XmlElement employee = doc.CreateElement("employee");
+
+            XmlElement nameElement = doc.CreateElement("name");
+            nameElement.InnerText = "Jay Adams";
+            employee.AppendChild(nameElement);
+
+            XmlElement idElement = doc.CreateElement("id");
+
+            idElement.InnerText = "42";
+
+            employee.AppendChild(idElement);
+
+            root.AppendChild(employee);
+            doc.AppendChild(root);
+
+            // Cannot convert type 'string' to 'int'
+            // No explict cast available
+            //int id = (int)idElement.Value;
+
+            int id = Convert.ToInt32(idElement.Value);
+        }
+
+        public static void GetElments()
+        {
+            XmlElement root = m_doc.CreateElement("employees");
+            root.AppendChild(AddEmployee(1, "Gustavo Achong", DateTime.Parse("7/31/1996"), false));
+            root.AppendChild(AddEmployee(3, "Kim Abercrombie", DateTime.Parse("12/12/1997"), true));
+            root.AppendChild(AddEmployee(8, "Carla Adams", DateTime.Parse("2/6/1998"), false));
+            root.AppendChild(AddEmployee(9, "Jay Adams", DateTime.Parse("2/6/1998"), false));
+
+            m_doc.AppendChild(root);
+
+            foreach (XmlNode node in m_doc.DocumentElement.ChildNodes)
+            {
+                Console.WriteLine(node.ToString());
+            }
+        }
+
+        #endregion XmlElement Demo Old
     }
 }
