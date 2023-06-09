c007892cd438cfb35a3ee116dca73faac2e31a15
胡睿毅 <807776962@qq.com>
2023-06-10 05:45:49 +0800

    upgrade

---
+++ dotnet-samples/WebApi/SelfHostAPI/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace SelfHostAPI
+{
+    internal class Program
+    {
+        private static readonly Uri _baseAddress = new Uri("http://localhost:8082/");
+
+        private static void Main(string[] args)
+        {
+            HttpSelfHostServer server = null;
+            try
+            {
+                // Set up server configuration
+                HttpSelfHostConfiguration config = new HttpSelfHostConfiguration(_baseAddress);
+
+                config.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "api/{controller}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+                );
+
+                // Create server
+                server = new HttpSelfHostServer(config);
+
+                // Start listening
+                server.OpenAsync().Wait();
+                Console.WriteLine("Listening on " + _baseAddress);
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Could not start server: {0}", e.GetBaseException().Message);
+                Console.WriteLine("Hit ENTER to exit...");
+                Console.ReadLine();
+            }
+            finally
+            {
+                if (server != null)
+                {
+                    // Stop listening
+                    server.CloseAsync().Wait();
+                }
+            }
+        }
+    }
+}
