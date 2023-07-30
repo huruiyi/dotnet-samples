c8b9a594e2c1f8e1be73942823c9c428b4a34ba5
胡睿毅 <807776962@qq.com>
2023-07-30 20:00:24 +0800

    WebApi.OAuth

---
+++ dotnet-samples/WebApi/WebApi.OAuth/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApi.OAuth.Startup))]
+
+namespace WebApi.OAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
