48103baadee5a8d145d8a680b1ab498101137768
胡睿毅 <807776962@qq.com>
2024-04-02 13:14:32 +0800

    wcf

---
+++ wcf/wcf/02JSONWCFService/AddService.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02JSONWCFService
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码、svc 和配置文件中的类名“AddService”。
+    // 注意: 为了启动 WCF 测试客户端以测试此服务，请在解决方案资源管理器中选择 AddService.svc 或 AddService.svc.cs，然后开始调试。
+    public class AddService : IAddService
+    {
+        public decimal Add(string Number1, string Number2, out string error)
+        {
+            decimal result = 0;
+            error = null;
+            try
+            {
+                result = Convert.ToDecimal(Number1) + Convert.ToDecimal(Number2);
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+            }
+
+            return result;
+        }
+    }
+}
